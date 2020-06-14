93e47a60cda490ab4b2860507dac128ce896e1a5
shibu130 <51987227+shibu130@users.noreply.github.com>
2020-06-14 21:51:02 +0530

    added files

---
+++ visa_masterlist/visa.py
@@ -0,0 +1,86 @@
+import mysql.connector
+with open("visa.txt","r") as  f :
+
+
+    line=f.readlines()
+
+stack=[]
+dictionary={}
+values=[]
+j=1
+
+
+
+for i in line:
+    if j==1:
+        dictionary[i.strip()]=[]
+        j=j+1
+        print("first standalone")
+    elif '{' in i:
+        print("opening")
+        stack.append('{')
+    elif '}' in i:
+        print("closing")
+        stack=[]
+    else:
+        if '{' in stack:
+            a=list(dictionary.keys())[-1]
+            values.append(i.strip())
+            dictionary[a]=values
+        else:
+            print("standalone")
+            values=[]
+            dictionary[i.strip()]=[]
+        
+
+        
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="imrankhan",
+  passwd="imukhan@25",
+  database="user"
+)
+mycursor=mydb.cursor()
+           
+
+
+
+#print(dictionary)
+#count=0
+for f,g in dictionary.items():
+  #  print(f)
+    for u in g:
+       # print(u)
+        sql = "INSERT INTO visatypes (country,visa) VALUES (%s, %s)"
+        val = (f,u)
+        mycursor.execute(sql, val)
+     #   count=count+1
+        mydb.commit()
+
+
+
+#print(count)
+
+
+
+
+
+
+# mydb = mysql.connector.connect(
+#   host="localhost",
+#   user="imrankhan",
+#   passwd="imukhan@25",
+#   database="user"
+# )
+
+
+# cursor=mydb.cursor()
+
+
+
+#print(dictionary.keys())
+
+# d={"one":"","two":""}
+
+# a=list(d.keys())[-1]
+# print(a)
